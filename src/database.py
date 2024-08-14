374376d2d36876216b469e49a08c709a0ed42495
Egor-Zharskiy <egor.zharskiy@mail.ru>
2024-08-14 18:57:00 +0300

    added .env file for db and docker-compose.yaml

--- src/database.py
+++ src/database.py
@@ -1,4 +1,8 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
-client = MongoClient("localhost", 27017)
-db = client['parser_db']
+load_dotenv()
+
+client = MongoClient(os.getenv('DB_HOST'), int(os.getenv('DB_PORT')))
+db = client[os.getenv('DB_NAME')]
