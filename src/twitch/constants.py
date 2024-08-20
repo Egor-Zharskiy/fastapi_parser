99298d83732c177538ed4c5191082d42672278bc
Egor-Zharskiy <egor.zharskiy@mail.ru>
2024-08-21 00:08:48 +0300

    added constants for parsers urls

---
+++ src/twitch/constants.py
@@ -0,0 +1,4 @@
+game_url = "https://api.twitch.tv/helix/games"
+stream_url = "https://api.twitch.tv/helix/streams"
+user_url = "https://api.twitch.tv/helix/users"
+token_url = "https://id.twitch.tv/oauth2/token"
